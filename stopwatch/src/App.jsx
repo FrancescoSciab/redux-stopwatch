1ec7983be97ac4bca7b370967f8a46c2a5581e37
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-08-05 12:12:02 +0100

    basic no-ui setup

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>
+          Redux Stopwatch
+        </h1>
+
+        <div className='running-time'>
+          00:01:49
+        </div>
+
+        <div>
+          <button>+</button>
+          <button>Start</button>
+          <button>Stop</button>
+          <button>Lap</button>
+          <button>Reset</button>
+          <button>-</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
